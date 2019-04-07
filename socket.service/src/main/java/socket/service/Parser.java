d7a26d219e7800450aff7b4c472b869777d1f65c
martkrisz <martinkovics.krisztian@gmail.com>
2019-04-07 20:55:26 +0200

    fix

--- socket.service/src/main/java/socket/service/Parser.java
+++ socket.service/src/main/java/socket/service/Parser.java
@@ -6,8 +6,9 @@ import javax.json.*;
 public class Parser {
 	
 	public JsonObject getJsonObject(String message) {
+		message.replaceAll("\r?\n", "");
 		JsonReader reader = Json.createReader(new StringReader(message));
-		JsonObject result = reader.readObject();
+		JsonObject result = reader.readValue().asJsonObject();
 		reader.close();
 		return result;
 	}
